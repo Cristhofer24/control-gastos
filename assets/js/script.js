81d8a13fd895bbf05772ac709d845866bb718177
cristhofer24 <you@example.com>
2024-09-19 10:05:11 -0500

    Agregado boton actualizar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,62 +1,70 @@
-let listaNomGastos=[];
-let listaValGastos=[];
+let listaNomGastos = [];
+let listaValGastos = [];
+let editIndex = -1; 
 
-function clickBoton(){
-    let gasto= document.getElementById("nombreGasto").value;
-    let valor= document.getElementById("valorGasto").value;
+function clickBolton() {
+    let gasto = document.getElementById("nombreGasto").value;
+    let valor = document.getElementById("valorGasto").value;
 
-console.log(gasto,valor);
+    if (gasto === "" || valor === "") {
+        alert("Por favor, ingresa un nombre y un valor para el gasto.");
+        return;
+    }
 
+    if (editIndex === -1) {
+       
+        listaNomGastos.push(gasto);
+        listaValGastos.push(valor);
+    } else {
+        
+        listaNomGastos[editIndex] = gasto;
+        listaValGastos[editIndex] = valor;
+        editIndex = -1; 
+        document.getElementById("botonFormulario").innerText = "Agregar Gasto"; 
+    }
 
-    listaNomGastos.push(gasto);
-    listaValGastos.push(valor);
-
-    console.log(listaNomGastos,listaValGastos);
-
-    actualizarGastos()
-
-
+    actualizarGastos();
 }
 
-function actualizarGastos(){
-    const listaElement=document.getElementById("listaDeGastos");
-    const totalElemt=document.getElementById("totalGastos");
+function actualizarGastos() {
+    const listaElement = document.getElementById("listaDeGastos");
+    const totalElemt = document.getElementById("totalGastos");
 
-    let total='';
-    let totalGastos=0;
-    listaNomGastos.forEach((elementos,posicion) =>{
-     const valorGasto = Number(listaValGastos[posicion])
-        total+=`<li>${elementos} - USD ${valorGasto}
-         <button onclick="eliminar(${posicion})">Eliminar</button></li>`;
-        totalGastos+=Number(valorGasto);
-    }); 
-    listaElement.innerHTML=total; 
-    totalElemt.innerHTML=totalGastos.toFixed(2);
+    let total = '';
+    let totalGastos = 0;
+    listaNomGastos.forEach((elementos, posicion) => {
+        const valorGasto = Number(listaValGastos[posicion]);
+        total += `<li>${elementos} - USD ${valorGasto}
+         <button onclick="eliminar(${posicion})">Eliminar</button>
+         <button onclick="editar(${posicion})">Editar</button></li>`;
+        totalGastos += valorGasto;
+    });
+    listaElement.innerHTML = total;
+    totalElemt.innerHTML = totalGastos.toFixed(2);
+
+    resetear();
+}
+
+function resetear() {
+    document.getElementById("nombreGasto").value = "";
+    document.getElementById("valorGasto").value = "";
+}
+
+function eliminar(posicion) {
+    listaNomGastos.splice(posicion, 1);
+    listaValGastos.splice(posicion, 1);
+
+    actualizarGastos();
+}
+
+function editar(posicion) {
     
-     resetear();
+    document.getElementById("nombreGasto").value = listaNomGastos[posicion];
+    document.getElementById("valorGasto").value = listaValGastos[posicion];
+
     
+    document.getElementById("botonFormulario").innerText = "Editar Gasto";
+
+  
+    editIndex = posicion;
 }
-
-function resetear(){
-    document.getElementById("nombreGasto").value="";
-    document.getElementById("valorGasto").value="";
-
-}
-
-function eliminar(posicion){
-    listaNomGastos.splice(posicion,1);
-    listaValGastos.splice(posicion,1);
-
-    actualizarGastos()
-    
-
-}
-
-
-///Desafios 
-//mensaje cuando los gastos sean mayores a 150
-//boton editar
-//detalle en los inputs
-
-// Validacion de campos si estan vacios */
-//Almacenamiento en una base de datos sql local
